8789faa1252efc70c4a0e7f7db070548ff22d9f6
CRACKBOY <CRACKBOY2006@gmail.com>
2014-05-28 15:12:31 +0100

    First commit
    Signed-off-by: CRACKBOY <CRACKBOY2006@gmail.com>

---
+++ integrations-demo/src/edu/esprit/integration/demo/managed/beans/CalculatorManagedBean.java
@@ -0,0 +1,9 @@
+package edu.esprit.integration.demo.managed.beans;
+
+public class CalculatorManagedBean {
+
+	public CalculatorManagedBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ integrations-demo/src/edu/esprit/integration/demo/managed/services/CalculatorService.java
@@ -0,0 +1,9 @@
+package edu.esprit.integration.demo.managed.services;
+
+public class CalculatorService {
+
+	public CalculatorService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
